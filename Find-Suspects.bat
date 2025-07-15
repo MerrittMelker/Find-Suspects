c89caf0707621bc0644249c7b15cde2d1ada81fe
Merritt Melker <merrittmelker@gmail.com>
2025-07-15 16:28:16 -0500

    Update Find-Suspects.bat

--- Find-Suspects.bat
+++ Find-Suspects.bat
@@ -1,2 +1,5 @@
 @echo off
-powershell -ExecutionPolicy Bypass -File Find-Suspects.ps1 -CommitSha %1
+powershell -ExecutionPolicy Bypass -File "%~dp0Find-Suspects.ps1" -CommitSha %1
+echo.
+echo Script finished. Press any key to exit.
+pause > nul
